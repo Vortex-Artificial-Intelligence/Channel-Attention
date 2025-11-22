6ebb15e51232f5379da949df387c70d114643883
wwhenxuan <wwhenxuan@gmail.com>
2025-11-22 12:09:34 +0800

    wheneuxan: update the utils

--- channel_attention/utils.py
+++ channel_attention/utils.py
@@ -12,7 +12,20 @@ def create_conv_layer(
     bias: bool = True,
     groups: int = 1,
 ) -> nn.Module:
-    """Create a convolutional layer based on the number of dimensions."""
+    """
+    Create a convolutional layer based on the number of dimensions.
+    
+    :param n_dims: The number of dimensions (1 or 2).
+    :param in_channels: Number of input channels.
+    :param out_channels: Number of output channels.
+    :param kernel_size: Size of the convolutional kernel.
+    :param stride: Stride of the convolution. Default is 1.
+    :param padding: Padding added to both sides of the input. Default is 0.
+    :param bias: If True, adds a learnable bias to the output. Default is True.
+    :param groups: Number of blocked connections from input channels to output channels. Default is 1.
+    
+    :return: A convolutional layer (nn.Conv1d or nn.Conv2d).
+    """
     if n_dims == 1:
         return nn.Conv1d(
             in_channels=in_channels,
